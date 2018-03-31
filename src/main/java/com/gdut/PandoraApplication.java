7effb9b264d0ae9896e68028a48c1b8a807391c8
buzheng <yupeibiaodeveloper@yeah.net>
2018-03-31 15:49:23 +0800

    初始化工程

---
+++ src/main/java/com/gdut/PandoraApplication.java
@@ -0,0 +1,12 @@
+package com.gdut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandoraApplication.class, args);
+	}
+}
