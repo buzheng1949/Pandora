545440462edbc73a814c77406b8041856a393080
buzheng <yupeibiaodeveloper@yeah.net>
2018-03-31 16:13:45 +0800

    增加用户的SQL以及实体类以及MyBatis配置

---
+++ src/main/java/com/gdut/pandora/domain/User.java
@@ -0,0 +1,27 @@
+package com.gdut.pandora.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private Integer createTime;
+
+    private Integer updateTime;
+
+}
